c89521217415c2667ed0501f1ae71c44e3943e68
Anna Strik <anna.strik1@gmail.com>
2020-03-16 19:07:59 -0700

    header tests 1st link

--- modules/header.spec.js
+++ modules/header.spec.js
@@ -4,15 +4,14 @@ import {expect} from "chai";
 describe('TEST ASANA', () => {
     before('go to asana.com', () => {
         browser.url('https://www.asana.com/');
-        browser.refresh();
-        browser.pause(2000);
-        expect(browser.$('//div[@class="siteHeader__row"]').isDisplayed()).true;
+        browser.maximizeWindow();
     });
 
     it('should verify header items', () =>{
         browser.$('//button[@id="navigation__dropdown-toggle-why-asana"]').click();
         browser.pause(500);
         browser.$('//h4[contains(text(), "Asana Overview")]').click();
+        expect(($('//h1').getText()).includes("The easiest way to manage team projects and tasks")).true;
         browser.pause(500);
     });
 });
